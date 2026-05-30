4e04d449201c25ca92064e5f853376a38bb9dbd1
Alex Buly <asbuly@icloud.com>
2026-05-29 21:26:27 -0400

    Add upload path

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const fileController = require("../controllers/fileController.js");
 const fileRouter = Router();
+const upload = require("../middleware/upload");
 
 fileRouter.get("/", fileController.getUploader);
+fileRouter.post("/", upload.single("file"), fileController.fileUpload);
 
 module.exports = fileRouter;
